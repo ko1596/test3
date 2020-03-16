74e2bac235ae3ad399616f5cb2b6d6f06bc49fde
ko1596 <ko89022189@gmail.com>
2020-03-16 14:59:07 +0800

    第二次修改

--- GitTutorial/GitTutorial/gitTutorial.cpp
+++ GitTutorial/GitTutorial/gitTutorial.cpp
@@ -1,10 +1,16 @@
-
+#include "pch.h"
 #include <string>
 #include <iostream>
 using namespace System;
 bool Guess(int number)
 {
+	return false;
 }
 int main(array<System::String^>^ args)
 {
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
